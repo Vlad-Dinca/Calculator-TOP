bb5a2e256b53288e3f2175a032bf15903bb1d5d9
Vlad Dincă <dincavlad01@gmail.com>
2025-11-28 08:02:25 +0200

    Format documents to Spaces:2

--- javascript.js
+++ javascript.js
@@ -1,40 +1,40 @@
-function operator(a, b, op){
-    switch (op){
-        case "+": 
-            return add(a, b);
-            break;
+function operator(a, b, op) {
+  switch (op) {
+    case "+":
+      return add(a, b);
+      break;
 
-        case "-": 
-            return subtract(a, b);
-            break;
+    case "-":
+      return subtract(a, b);
+      break;
 
-        case "*": 
-            return multiply(a, b);
-            break;
+    case "*":
+      return multiply(a, b);
+      break;
 
-        case "/": 
-            return divide(a, b);
-            break;
-        
-        default:
-            return "ERROR!";
-    }
+    case "/":
+      return divide(a, b);
+      break;
+
+    default:
+      return "ERROR!";
+  }
 }
 
-function add(a, b){
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a, b){
-    return a - b;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a, b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a, b){
-    return a / b;
+function divide(a, b) {
+  return a / b;
 }
 
 let a = -3;
