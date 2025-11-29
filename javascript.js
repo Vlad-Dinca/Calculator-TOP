c79e4f724784c36e8236b0b6f16a328a4f5d29ad
Vlad Dincă <dincavlad01@gmail.com>
2025-11-29 08:31:36 +0200

    Fix bugs
    - Maximum number of digits on display is 14.
    - Decimals have only 2 digits.
    - Can't spam 0.
    - Can't spam operator buttons.

--- javascript.js
+++ javascript.js
@@ -22,7 +22,6 @@ function operator(a, b, op) {
 }
 
 function add(a, b) {
-  console.log(a+b);
   return a + b;
 }
 
@@ -31,8 +30,6 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-  console.log(typeof(a) + typeof(b));
-  console.log(a + b);
   return a * b;
 }
 
@@ -40,134 +37,171 @@ function divide(a, b) {
   return a / b;
 }
 
-function clickAction(action){
+function clickAction(action) {
 
 }
 
 const result = document.querySelector("#resultButton");
-const reset = document.querySelector("#resetButton") ;
-const zero = document.querySelector("#zeroButton") ;
-const one = document.querySelector("#oneButton") ;
-const two = document.querySelector("#twoButton") ;
-const three = document.querySelector("#threeButton") ;
-const four = document.querySelector("#fourButton") ;
-const five = document.querySelector("#fiveButton") ;
-const six = document.querySelector("#sixButton") ;
-const seven = document.querySelector("#sevenButton") ;
-const eight = document.querySelector("#eightButton") ;
-const nine = document.querySelector("#nineButton") ;
-const addButton = document.querySelector("#addButton") ;
-const subtractButton = document.querySelector("#subtractButton") ;
-const multiplyButton = document.querySelector("#multiplyButton") ;
-const divideButton = document.querySelector("#divideButton") ;
+const reset = document.querySelector("#resetButton");
+const zero = document.querySelector("#zeroButton");
+const one = document.querySelector("#oneButton");
+const two = document.querySelector("#twoButton");
+const three = document.querySelector("#threeButton");
+const four = document.querySelector("#fourButton");
+const five = document.querySelector("#fiveButton");
+const six = document.querySelector("#sixButton");
+const seven = document.querySelector("#sevenButton");
+const eight = document.querySelector("#eightButton");
+const nine = document.querySelector("#nineButton");
+const addButton = document.querySelector("#addButton");
+const subtractButton = document.querySelector("#subtractButton");
+const multiplyButton = document.querySelector("#multiplyButton");
+const divideButton = document.querySelector("#divideButton");
 const display = document.querySelector("#display");
-display.innerText = "0";
-let displayString ="";
-let firstNumber ="";
-let secondNumber ="";
+display.innerText = "";
+let displayString = "";
+let firstNumber = 0;
+let secondNumber = 0;
 let op = "";
+let pressed = false; 
 
-if (displayString.length <= 9){
-  zero.addEventListener(("click"), function(event){ 
-    display.innerText="0"; 
+
+zero.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) { // first digit can't be 0, unless we add, multiply, subtract or divide
+                                                                   // maximum size of a number can pe 9 digits
+    display.innerText = "0";
     displayString += display.innerText;
     display.innerText = displayString;
-  });
-
-  one.addEventListener(("click"), function(event){ 
-    display.innerText="1"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-
-  two.addEventListener(("click"), function(event){ 
-    display.innerText="2"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-
-  three.addEventListener(("click"), function(event){ 
-    display.innerText="3"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-
-  four.addEventListener(("click"), function(event){ 
-    display.innerText="4"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-
-  five.addEventListener(("click"), function(event){ 
-    display.innerText="5"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-
-  six.addEventListener(("click"), function(event){ 
-    display.innerText="6"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-
-  seven.addEventListener(("click"), function(event){ 
-    display.innerText="7"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-
-  eight.addEventListener(("click"), function(event){ 
-    display.innerText="8"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-
-  nine.addEventListener(("click"), function(event){ 
-    display.innerText="9"; 
-    displayString += display.innerText;
-    display.innerText = displayString;
-  });
-};
-
-addButton.addEventListener(("click"), function(event){
-  firstNumber = parseFloat(display.innerText).toFixed(2);
-  display.innerText="";
-  displayString="";
-  op = "+"
+  } 
 });
 
-subtractButton.addEventListener(("click"), function(event){
-  firstNumber = parseFloat(display.innerText).toFixed(2);
-  display.innerText="";
-  displayString="";
-  op = "-"
+one.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "1";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
 });
 
-multiplyButton.addEventListener(("click"), function(event){
-  firstNumber = parseFloat(display.innerText).toFixed(2);
-  display.innerText="";
-  displayString="";
-  op = "*"
+two.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "2";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
 });
 
-divideButton.addEventListener(("click"), function(event){
-  firstNumber = parseFloat(display.innerText).toFixed(2);
-  display.innerText="";
-  displayString="";
-  op = "/"
-})
-
-
-result.addEventListener(("click"), function(event){
-  secondNumber = parseFloat(display.innerText).toFixed(2);
-  display.innerText=(operator(firstNumber, secondNumber, op)).toString(); 
+three.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "3";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
 });
 
-reset.addEventListener(("click"), function(event){
-  display.innerText="0";
+four.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "4";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
+});
+
+five.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "5";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
+});
+
+six.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "6";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
+});
+
+seven.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "7";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
+});
+
+eight.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "8";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
+});
+
+nine.addEventListener(("click"), function (event) {
+  if (display.innerText[0] != 0 && display.innerText.length < 9) {
+    display.innerText = "9";
+    displayString += display.innerText;
+    display.innerText = displayString;
+  }
+});
+
+
+addButton.addEventListener(("click"), function (event) {
+  if (pressed === false && display.innerText !=""){
+    firstNumber = parseFloat(display.innerText);
+    display.innerText = "";
+    displayString = "";
+    op = "+"
+    pressed = true; // once you press one of the operator buttons, you are locked out of the other operator buttons
+  }
+});
+
+subtractButton.addEventListener(("click"), function (event) {
+  if (pressed === false && display.innerText !="") {
+    firstNumber = parseFloat(display.innerText);
+    display.innerText = "";
+    displayString = "";
+    op = "-"
+    pressed = true;
+  }
+});
+
+multiplyButton.addEventListener(("click"), function (event) {
+  if (pressed === false && display.innerText !="") {
+    firstNumber = parseFloat(display.innerText);
+    display.innerText = "";
+    displayString = "";
+    op = "*"
+    pressed = true;
+  }
+});
+
+divideButton.addEventListener(("click"), function (event) {
+  if (pressed === false  && display.innerText !="") {
+    firstNumber = parseFloat(display.innerText);
+    display.innerText = "";
+    displayString = "";
+    op = "/"
+    pressed = true;
+  }
+});
+
+
+
+result.addEventListener(("click"), function (event) {
+  secondNumber = parseFloat(display.innerText);
+  display.innerText = (operator(firstNumber, secondNumber, op)).toFixed(2).substr(0, 14); // show a maximum of 14 digits on the display.
+  pressed = false;
+});
+
+reset.addEventListener(("click"), function (event) {
+  display.innerText = "";
   firstNumber = 0;
   secondNumber = 0;
   displayString = "";
+  pressed = false;
 })
 
 
